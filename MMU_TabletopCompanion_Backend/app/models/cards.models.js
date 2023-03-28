77ee11bd47f0a4a9c00f96f616aac94b63d3b886
zWarnockTreece <21302198@stu.mmu.ac.uk>
2023-03-28 13:38:08 +0100

    Local API
    Implemented a local version of most/all of the API's functionality

--- MMU_TabletopCompanion_Backend/app/models/cards.models.js
+++ MMU_TabletopCompanion_Backend/app/models/cards.models.js
@@ -4,35 +4,185 @@ const db = require('../../database');
 const pokemonSDK = require('pokemontcgsdk');
 pokemonSDK.configure({apiKey: API_KEY});
 
-const searchForCards = (filters, pageNumber, done) => {
-    let filterQuery = "";
+const searchForCards = (filters, nestedFilters, flags, done) => {
+    let sql = "SELECT id FROM Cards";
+    let values = [];
 
-    filters.forEach(filter => {
-        filterQuery += filter;
-        filterQuery += " ";
-    });
+    console.log(filters)
+    console.log(nestedFilters)
+    console.log(flags)
 
+    if(nestedFilters.hasOwnProperty('types')){
+        if(flags.typesExclusive){
+            for(let i = 0; i < nestedFilters.types.length; i++){       
+                sql += ' INNER JOIN Card_Types as type' + i
+                sql += ' ON Cards.id = type' + i + '.cardID'
+                sql += ' AND type' + i + '.name = ?'
+                values.push(nestedFilters.types[i]);
+            }
+        }else{
+            sql += ' INNER JOIN Card_Types as types'
+            sql += ' ON Cards.id = types.cardID'
+            sql += ' AND (types.name = ?'
+            values.push(nestedFilters.types[0]);
+            if(nestedFilters.types.length > 1){
+                for(let i = 1; i < nestedFilters.types.length; i++){
+                    sql += ' OR types.name = ?'
+                    values.push(nestedFilters.types[i]);
+                }
+            }
+            sql += ')'
+        }
+    }
+
+    if(nestedFilters.hasOwnProperty('negatedTypes')){
+        if(flags.negatedSubtypesExclusive){
+            for(let i = 0; i < nestedFilters.negatedTypes.length; i++){       
+                sql += ' INNER JOIN Card_Types as negatedType' + i
+                sql += ' ON Cards.id = negatedType' + i + '.cardID'
+                sql += ' AND NOT negatedType' + i + '.name = ?'
+                values.push(nestedFilters.negatedTypes[i]);
+            }
+        }else{
+            sql += ' INNER JOIN Card_Types as negatedTypes'
+            sql += ' ON Cards.id = negatedTypes.cardID'
+            sql += ' AND NOT (negatedTypes.name = ?'
+            values.push(nestedFilters.negatedTypes[0]);
+            if(nestedFilters.negatedTypes.length > 1){
+                for(let i = 1; i < nestedFilters.negatedTypes.length; i++){
+                    sql += ' OR negatedTypes.name = ?'
+                    values.push(nestedFilters.negatedTypes[i]);
+                }
+            }
+            sql += ')'
+        }
+    }
+
+
+    if(nestedFilters.hasOwnProperty('subtypes')){
+        if(flags.subtypesExclusive){
+            for(let i = 0; i < nestedFilters.subtypes.length; i++){       
+                sql += ' INNER JOIN Card_Subtypes as subtypes' + i
+                sql += ' ON Cards.id = subtypes' + i + '.cardID'
+                sql += ' AND subtypes' + i + '.name = ?'
+                values.push(nestedFilters.subtypes[i]);
+            }
+        }else{
+            sql += ' INNER JOIN Card_Subtypes as subtypes'
+            sql += ' ON Cards.id = subtypes.cardID'
+            sql += ' AND (subtypes.name = ?'
+            values.push(nestedFilters.subtypes[0]);
+            if(nestedFilters.subtypes.length > 1){
+                for(let i = 1; i < nestedFilters.subtypes.length; i++){
+                    sql += ' OR subtypes.name = ?'
+                    values.push(nestedFilters.subtypes[i]);
+                }
+            }
+            sql += ')'
+        }
+    }
+    if(nestedFilters.hasOwnProperty('negatedSubtypes')){
+        if(flags.negatedSubtypesExclusive){
+            for(let i = 0; i < nestedFilters.negatedSubtypes.length; i++){       
+                sql += ' INNER JOIN Card_Subtypes as negatedSubtypes' + i
+                sql += ' ON Cards.id = negatedSubtypes' + i + '.cardID'
+                sql += ' AND NOT negatedSubtypes' + i + '.name = ?'
+                values.push(nestedFilters.negatedSubtypes[i]);
+            }
+        }else{
+            sql += ' INNER JOIN Card_Subtypes as negatedSubtypes'
+            sql += ' ON Cards.id = negatedSubtypes.cardID'
+            sql += ' AND NOT (negatedSubtypes.name = ?'
+            values.push(nestedFilters.negatedSubtypes[0]);
+            if(nestedFilters.negatedSubtypes.length > 1){
+                for(let i = 1; i < nestedFilters.negatedSubtypes.length; i++){
+                    sql += ' OR negatedSubtypes.name = ?'
+                    values.push(nestedFilters.negatedSubtypes[i]);
+                }
+            }
+            sql += ')'
+        }
+    }
+
+    if(nestedFilters.hasOwnProperty('legality')){
+        sql += ' INNER JOIN Card_Legalities'
+        sql += ' ON Cards.id = Card_Legalities.cardID'
+        sql += ' AND Card_Legalities.' + nestedFilters.legality.name + ' = ?'
+        values.push(nestedFilters.legality.level);
+    }
+
+    if(filters.hasOwnProperty('name')){
+        sql += ' WHERE Cards.name LIKE ?';
+        values.push(filters.name.replaceAll('*', '%'));
+    }
+
+    if(filters.hasOwnProperty('supertype')){
+        sql += ' AND supertype = ?'
+        values.push(filters.supertype);
+    }
+
+    console.log(sql)
+    console.log(values)
+
+    let ids = [];
     let results = [];
+    db.each(
+        sql,
+        values,
+        (err, row) => {
+            if(err){
+                console.log("Something went wrong: " + err);
+                return done(err, null);
+            }
+            ids.push(row.id);
+        },
+        (err, num_rows) => {
+            //Remove any potential duplicate card values due to multiple matching nested filters and update result count
+            ids = [...new Set(ids)];
+            num_rows = ids.length
+            console.log(num_rows)
+            //Calculate total pages, return error if invalid page number in filters
+            let pageCount = Math.ceil(num_rows/250);
+            if(filters.page > pageCount || filters.page < 1){
+                console.log("Invalid Page Number");
+                return done(null, [], num_rows);
+            }
 
-    pokemonSDK.card.where({q: filterQuery, pageSize: 250, page: pageNumber})
-    .then(result => {
-        if(result.error) return res.status(result.error).send(error);
+            //Offset for search based on page number
+            let i = (filters.page - 1) * 250;
+            if(filters.page > 1) i--;
 
-        result.data.forEach(card => {
-            results.push(card);
-        });
-        return done(null, results);
-    });
+            //Logic to handle cases where less than 250 results would be returned
+            let iLim;
+            if(num_rows - ((filters.page - 1) * 250) < 250) iLim = i + (num_rows - ((filters.page - 1) * 250));
+            else iLim = i + 250;
+            let targetResults = iLim - i;
+
+            let counter = 0;
+            for(i; i < iLim; i++){
+                getCardByID(ids[i], (err, data) => {
+                    if(err){
+                        console.log("Something went wrong:" + err);
+                        return done(err, null, null);
+                    }
+                    results.push(data);
+                    counter++;
+                    if(counter == targetResults) return done(null, results, num_rows);
+                })
+            }
+
+        }
+    );
 }
 
 const getCardByID = (id, done) => {
-    const sql = 'SELECT * FROM cards WHERE card_id=?';
+    const sql = 'SELECT data FROM Cards_Cached WHERE id=?';
 
     db.get(sql, [id], (err, row) => {
         if(err) return done(err);
         if(!row) return done(404);
 
-        return done(null, JSON.parse(row.card_info));
+        return done(null, JSON.parse(row.data));
     });
 }
 
--- MMU_TabletopCompanion_Backend/database.refresh.js
+++ MMU_TabletopCompanion_Backend/database.refresh.js
@@ -13,11 +13,267 @@ for(let i = 2; i < process.argv.length; i++){if(flags.hasOwnProperty(process.arg
 
 const addCards = (cards) => {
         console.log("Retrieved " + cards.length + " cards");
-        cards.forEach(card => {
-                const sql = 'INSERT INTO cards (card_id, card_info) VALUES (?,?)';
-                db.run(sql, [card.id, JSON.stringify(card)], (err) => {
-                        if(err) console.log("Failed to add " + card.name + "\nReason: " + err);
+        cards.forEach((card => {
+                let sql = 'INSERT INTO Cards_Cached (id, data) VALUES (?,?)';
+                values = [card.id, JSON.stringify(card)];
+                db.run(sql, values, (err) => {
+                        if(err) console.log("Failed to add " + card.name + "'s cache data.");
                 });
+
+                addCard(card);
+                addEvolutions(card);
+                addSubtypes(card);
+
+                addTypes(card);
+                addWeaknesses(card);
+                addResistances(card);
+
+                addAncientTrait(card);
+                addAbilities(card);
+                addAttacks(card);
+
+                addRules(card);
+                addLegality(card);
+                addPrices(card);
+        }))
+}
+
+const addCard = (card) => {
+        let sql = 'INSERT INTO Cards (id, setName, number, name, supertype, level, hp, evolvesFrom, retreatCost, convertedRetreatCost, artist, rarity, flavourText, pokedexNumbers, regulationMark, smallImage, largeImage) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+        let values = [card.id, card.set.name, card.number, card.name, card.supertype, card.level, card.hp, card.evolvesFrom, card.retreatCost, card.convertedRetreatCost, card.artist, card.rarity, card.flavorText, JSON.stringify(card.nationalPokedexNumbers), card.regulationMark, card.images.small, card.images.large];
+        db.run(sql, values, (err) => {
+                if(err) console.log("Failed to add " + card.name + "'s data.");
+        });
+}
+const addEvolutions = (card) => {
+        if(!(card.evolvesTo === undefined)){
+                card.evolvesTo.forEach(evolution => {
+                        sql = 'INSERT INTO Card_Evolutions (cardID, name) VALUES (?, ?)';
+                        values = [card.id, evolution];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + card.name + "'s evolutions.");
+                        });
+                });
+        }
+}
+const addSubtypes = (card) => {
+        if(!(card.subtypes === undefined)){
+                card.subtypes.forEach(subtype => {
+                        sql = 'INSERT INTO Card_Subtypes (cardID, name) VALUES (?, ?)';
+                        values = [card.id, subtype];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + card.name + "'s subtypes.");
+                        });
+                });
+        }
+}
+
+const addTypes = (card) => {
+        if(!(card.types === undefined)){
+                card.types.forEach(type => {
+                        sql = 'INSERT INTO Card_Types (cardID, name) VALUES (?, ?)';
+                        values = [card.id, type];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + card.name + "'s types.");
+                        });
+                });
+        }
+}
+const addWeaknesses = (card) => {
+        if(!(card.weaknesses === undefined)){
+                card.weaknesses.forEach(weakness => {
+                        sql = 'INSERT INTO Card_Weaknesses (cardID, name, value) VALUES (?, ?, ?)';
+                        values = [card.id, weakness.type, weakness.value];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + card.name + "'s weaknesses.");
+                        });
+                });
+        }
+}
+const addResistances = (card) => {
+        if(!(card.resistances === undefined)){
+                card.resistances.forEach(resistance => {
+                        sql = 'INSERT INTO Card_Resistances (cardID, name, value) VALUES (?, ?, ?)';
+                        values = [card.id, resistance.type, resistance.value];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + card.name + "'s resistances.");
+                        });
+                });
+        }
+}
+
+const addAncientTrait = (card) => {
+        if(!(card.ancientTrait === undefined)){
+                sql = 'INSERT or IGNORE INTO Ancient_Traits (name, text) VALUES (?, ?)';
+                        values = [card.ancientTrait.name, card.ancientTrait.text];
+
+                        db.run(sql, values, (err) => {
+                                //Will ignore failures related to the unique constraint (multiple cards reference the same trait)
+                                if(err) console.log("Failed to add " + card.ancientTrait.name);
+
+
+                                sql = 'INSERT INTO Card_Ancient_Traits (cardID, name) VALUES (?, ?)';
+                                values = [card.id, card.ancientTrait.name];
+
+                                db.run(sql, values, (err) => {
+                                        if(err) console.log("Failed to add " + card.name + "'s ancient trait");
+                                })
+                        })
+        }
+}
+const addAbilities = (card) => {
+        if(!(card.abilities === undefined)){
+                card.abilities.forEach(ability => {
+
+                        sql = 'INSERT or IGNORE INTO Abilities (name, text, type) VALUES (?, ?, ?)';
+                        values = [ability.name, ability.text, ability.type];
+
+                        db.run(sql, values, (err) => {
+                                //Will ignore failures related to the unique constraint (multiple cards reference the same ability)
+                                if(err) console.log("Failed to add " + ability.name);
+
+
+                                sql = 'INSERT INTO Card_Abilities (cardID, name) VALUES (?, ?)';
+                                values = [card.id, ability.name];
+
+                                db.run(sql, values, (err) => {
+                                        if(err) console.log("Failed to add " + card.name + "'s ability");
+                                })
+                        })
+                });
+        }
+}
+const addAttacks = (card) => {
+        if(!(card.attacks === undefined)){
+                card.attacks.forEach(attack => {
+
+                        sql = 'INSERT or IGNORE INTO Attacks (name, cost, convertedEnergyCost, damage, text) VALUES (?, ?, ?, ?, ?)';
+                        values = [attack.name, attack.cost, attack.convertedEnergyCost, attack.damage, attack.text];
+
+                        db.run(sql, values, (err) => {
+                                //Will ignore failures related to the unique constraint (multiple cards reference the same attack)
+                                if(err) console.log("Failed to add " + attack.name);
+
+                                db.get('SELECT rowid FROM Attacks WHERE name = ? AND cost = ?', [attack.name, attack.cost], (err, attack) => {
+                                        sql = 'INSERT INTO Card_Attacks (cardID, attackID) VALUES (?, ?)';
+                                        values = [card.id, attack.rowid];
+
+                                        db.run(sql, values, (err) => {
+                                                if(err) console.log("Failed to add " + card.name + "'s attack");
+                                        })
+                                })
+                        })
+                });
+        }
+}
+
+const addRules = (card) => {
+        if(!(card.rules === undefined)){
+                card.rules.forEach(rule => {
+                        sql = 'INSERT or IGNORE INTO Rules (text) VALUES (?)';
+                        values = [rule];
+
+                        db.run(sql, values, (err) => {
+                                //Card swshp-SWSH296 contains 2 instances of the same rule (presumably unintended)
+                                //Will also ignore failures related to the unique constraint (multiple cards reference the same rule)
+                                if(err && card.id != "swshp-SWSH296") console.log("Failed to add " + rule);
+
+
+                                        sql = 'INSERT INTO Card_Rules (cardID, text) VALUES (?, ?)';
+                                        values = [card.id, rule];
+
+                                        db.run(sql, values, (err) => {
+                                                if(err && card.id != "swshp-SWSH296") console.log("Failed to add " + card.name + "'s rule");
+                                        })
+                        })
+                });
+        }
+}
+const addLegality = (card) => {
+        sql = 'INSERT INTO Card_Legalities (cardID, standard, expanded, unlimited) VALUES (?, ?, ?, ?)';
+        values = [card.id, card.legalities.standard, card.legalities.expanded, card.legalities.unlimited];
+        db.run(sql, values, (err) => {
+                if(err) console.log("Failed to add " + card.name + "'s legalities.");
+        });
+}
+const addPrices = (card) => {
+        if(!(card.tcgplayer === undefined)){
+                sql = 'INSERT INTO Card_TCGPlayer_Data (id, URL, normal, holofoil, reverseHolofoil, firstEditionHolofoil, firstEditionNormal, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+                values = [card.id, card.tcgplayer.url, JSON.stringify(card.tcgplayer.prices.normal), JSON.stringify(card.tcgplayer.prices.holofoil), JSON.stringify(card.tcgplayer.prices.reverseHolofoil), JSON.stringify(card.tcgplayer.prices.firstEditionHolofoil), JSON.stringify(card.tcgplayer.prices.firstEditionNormal), card.tcgplayer.updatedAt];
+                db.run(sql, values, (err) => {
+                        if(err) console.log("Failed to add " + card.name + "'s TCGPlayer data.");
+                });
+        }
+
+        if(!(card.cardmarket === undefined)){
+                sql = 'INSERT INTO Cardmarket_Data (id, URL, data, updatedAt) VALUES (?, ?, ?, ?)';
+                values = [card.id, card.cardmarket.url, JSON.stringify(card.cardmarket.prices), card.cardmarket.updatedAt];
+                db.run(sql, values, (err) => {
+                        if(err) console.log("Failed to add " + card.name + "'s Cardmarket data.");
+                });
+        }
+}
+
+const addSets = () => {
+        pokemonSDK.set.all()
+        .then(result => {
+                if(result.error) console.log(error);
+                console.log("Retrieved " + result.length + " sets");
+                result.forEach(set => {
+                        let sql = 'INSERT INTO Sets (id, name, series, printedTotal, total, ptcgoCode, releaseDate, updatedAt, symbol, logo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+                        values = [set.id, set.name, set.series, set.printedTotal, set.total, set.ptcgoCode, set.releaseDate, set.updatedAt, set.images.symbol, set.images.logo];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + set.name + "'s resistances.");
+                        });
+
+                        sql = 'INSERT INTO Set_Legalities (setID, standard, expanded, unlimited) VALUES (?, ?, ?, ?)';
+                        values = [set.id, set.legalities.standard, set.legalities.expanded, set.legalities.unlimited];
+                        db.run(sql, values, (err) => {
+                                if(err) console.log("Failed to add " + set.name + "'s legalities.");
+                        });
+                });
+        })
+}
+
+const addData = () => {
+        pokemonSDK.type.all()
+        .then(types => {
+                types.forEach((type => {
+                        let sql = 'INSERT INTO Types (name) VALUES (?)';
+                        db.run(sql, [type], (err) => {
+                                if(err) console.log("Failed to add " + type);
+                        })
+                }))
+        });
+
+        pokemonSDK.subtype.all()
+        .then(subtypes => {
+                subtypes.forEach((subtype => {
+                        let sql = 'INSERT INTO Subtypes (name) VALUES (?)';
+                        db.run(sql, [subtype], (err) => {
+                                if(err) console.log("Failed to add " + subtype);
+                        })
+                }))
+        });
+
+        pokemonSDK.supertype.all()
+        .then(supertypes => {
+                supertypes.forEach((supertype => {
+                        let sql = 'INSERT INTO Supertypes (name) VALUES (?)';
+                        db.run(sql, [supertype], (err) => {
+                                if(err) console.log("Failed to add " + supertype);
+                        })
+                }))
+        });
+
+        pokemonSDK.rarity.all()
+        .then(rarities => {
+                rarities.forEach((rarity => {
+                        let sql = 'INSERT INTO Rarities (name) VALUES (?)';
+                        db.run(sql, [rarity], (err) => {
+                                if(err) console.log("Failed to add " + rarity);
+                        })
+                }))
         });
 }
 
@@ -33,20 +289,20 @@ const getCards = (pageNumber, cards) => {
                 console.log("Page " + pageNumber + " retrieved");
                 pageNumber++;
 
-                if(length >= 250){
-                        getCards(pageNumber, cards);
-                }else{
+                if(length < 250){
                         addCards(cards);
+                }else{
+                        getCards(pageNumber, cards);
                 }
         })
 }
 
-const sql = 'DELETE FROM decks';
+let sql = 'DELETE FROM decks';
 db.run(sql, [], function(err){
         if(err) throw err;
 
         console.log("Decks table: All data deleted");
-        const sql = 'DELETE FROM users WHERE username != ?';
+        let sql = 'DELETE FROM users WHERE username != ?';
 
         db.run(sql, [ADMIN_USERNAME], function(err){
                 if(err) throw err;
@@ -56,13 +312,23 @@ db.run(sql, [], function(err){
 });
 
 if(flags["-cards"]){
-        const sql = 'DELETE FROM cards';
-        db.run(sql, [], function(err){
-                if(err) throw err;
+        let tables = ["Cards_Cached", "Rarities", "Supertypes", "Subtypes", "Card_Subtypes", "Card_Evolutions", "Types", "Card_Types", "Card_Resistances", "Card_Weaknesses", "Rules", "Card_Rules", "Ancient_Traits", "Card_Ancient_Traits", "Abilities", "Card_Abilities", "Attacks", "Card_Attacks", "Card_Legalities", "Card_TCGPlayer_Data", "Cardmarket_Data", "Cards", "Sets", "Set_Legalities"]
 
-                console.log("Cards table: All data deleted");
-        });
+        let counter = 0;
+        tables.forEach((table => {
+                let sql = 'DELETE FROM ' + table;
 
-        let cards = [];
-        getCards(1, cards);
+                db.run(sql, [], function(err){
+                        if(err) throw err;
+                }); 
+
+                counter++;
+                if(counter == tables.length){
+                        console.log("All card data deleted");
+                        let cards = [];
+                        addSets();
+                        addData();
+                        getCards(1, cards);
+                }
+        }))
 }
