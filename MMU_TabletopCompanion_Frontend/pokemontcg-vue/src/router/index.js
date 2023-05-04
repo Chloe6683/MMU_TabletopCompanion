2de1a67b9829438dd6f93d8799a31de8263cef6d
Chloe <122321233+Chloe6683@users.noreply.github.com>
2023-05-04 14:14:42 +0100

    Decklist + Small Fixes

--- MMU_TabletopCompanion_Frontend/pokemontcg-vue/src/router/index.js
+++ MMU_TabletopCompanion_Frontend/pokemontcg-vue/src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory} from 'vue-router';
 import LandingPage from "../views/pages/LandingPage.vue"
 import Login from "../views/pages/Login.vue"
 import CreateDeck from "../views/pages/CreateDeck.vue"
+import DeckList from "../views/pages/DeckList.vue"
 
 const routes = [
     { path: "/",  component: LandingPage},
     { path: "/login", component: Login},
-    { path: "/create", component: CreateDeck}
+    { path: "/create", component: CreateDeck},
+    { path: "/decks", component: DeckList}
 ]
 
 const router = createRouter({
---
+++ MMU_TabletopCompanion_Frontend/pokemontcg-vue/src/views/components/DeckPreview.vue
@@ -0,0 +1,16 @@
+<template>
+        
+    <div class="alert alert-info">
+     <strong> {{ title }} </strong> {{ description }}
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        description: String
+    }
+}
+</script>
